bb6e703712f7ae924a6a8bb3283e350d7b97a254
sitota49 <se.sitota.ezra@gmail.com>
2020-06-26 19:22:24 +0300

    search bar implemented

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <SearchBar @termChange ="onTermChange"></SearchBar>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import SearchBar from './Components/SearchBar';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'App',
+    components:{
+        SearchBar 
+    },
+    methods: {
+        onTermChange(searchTerm){
+            console.log(searchTerm);
+        }
+    }
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
