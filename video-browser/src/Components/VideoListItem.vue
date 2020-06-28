a5a9288041829905bca529d72ab2dbf2c76dd50d
sitota49 <se.sitota.ezra@gmail.com>
2020-06-28 15:30:31 +0300

    styling

--- video-browser/src/Components/VideoListItem.vue
+++ video-browser/src/Components/VideoListItem.vue
@@ -1,12 +1,30 @@
 <template>
-    <li>
-       {{video.snippet.title}}
+    <li class="list-group-item media">
+        <img 
+            :src="thumbnailUrl"
+            class="mr-3">
+            <div class="media-body"> {{video.snippet.title}}</div>
+      
     </li>
 </template>
 
 <script>
 export default {
     name:'VideoListItem',
-    props:['video']
+    props:['video'],
+    computed:{
+        thumbnailUrl(){
+            return this.video.snippet.thumbnails.default.url
+        }
+    }
 }
 </script>
+<style scoped>
+    li {
+        display: flex;
+        cursor: pointer;
+    }
+    li:hover{
+        background: #eee;
+    }
+</style>
