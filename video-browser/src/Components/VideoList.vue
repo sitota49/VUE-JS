4ffce22d90aa2b80238ebe130741ed6e8c687849
sitota49 <se.sitota.ezra@gmail.com>
2020-06-28 15:06:13 +0300

    video list item

--- video-browser/src/Components/VideoList.vue
+++ video-browser/src/Components/VideoList.vue
@@ -1,16 +1,26 @@
 <template>
     <ul>
-        VideoList
-        {{videos.length}}
+        <VideoListItem
+
+        v-for="video in videos"
+        :video="video"
+        :key="video.etag"
+         
+        >
+
+            
+        </VideoListItem>
     </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
 export default {
     name:'VideoList',
-    props:{
-        videos: Array
-    }
+    components:{
+        VideoListItem
+    },
+    props:['videos']
 }
 </script>
 
