729eb4981b3ee96e345b9776a83ec2a701af2f8d
sitota49 <se.sitota.ezra@gmail.com>
2020-06-28 14:53:14 +0300

    video list

--- video-browser/src/App.vue
+++ video-browser/src/App.vue
@@ -1,21 +1,41 @@
 <template>
     <div>
         <SearchBar @termChange ="onTermChange"></SearchBar>
+        <VideoList :videos="videos"></VideoList>
+
     </div>
 </template>
 
 <script>
 
+import axios from 'axios';
 import SearchBar from './Components/SearchBar';
+import VideoList from './Components/VideoList'
+
+const API_KEY= 'AIzaSyCh_KKea5O3rtlxEG-KqIMWJHQ3lcZnLi0';
 
 export default {
     name: 'App',
     components:{
-        SearchBar 
+        SearchBar ,
+        VideoList
     },
+    data (){
+        return {videos:[]};
+    }
+    ,
     methods: {
         onTermChange(searchTerm){
-            console.log(searchTerm);
+            axios.get('https://www.googleapis.com/youtube/v3/search',{
+                params:{
+                    key: API_KEY,
+                    type:'video',
+                    part: 'snippet',
+                    q: searchTerm
+                }
+            }).then(response=>{
+                this.videos= response.data.items
+            });
         }
     }
 };
